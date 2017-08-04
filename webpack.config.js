fec8c257a9b5c4d9c68b67bcd400d4f9274e3d5f
Hebert Porto <hebertporto@gmail.com>
2017-08-04 13:28:53 -0300

    added arquivo de configuração do linter

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
         exclude: /node_modules/,
         query: {
           presets: ['es2015', 'react', 'stage-3'],
-        }
+        },
       },
       {
         use: ['style-loader', 'css-loader'],
